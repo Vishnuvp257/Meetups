503a17749c98aeb345a16897ab67ed66d29162f5
vishnu vp <vishnuprakaash257@gmail.com>
2022-10-08 20:37:19 +0530

    create new-meetup route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { MongoClient } from "mongodb";
+
+import { useRouter } from "next/router";
 
 import NewMeetupForm from "./../../components/meetups/NewMeetupForm";
 
 function NewMeetup(props) {
-  const submitHandler = (entered) => {
-    console.log(entered);
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredData) => {
+    const res = await fetch("/api/new-meetup/", {
+      method: "POST",
+      body: JSON.stringify(enteredData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    router.push("/");
   };
 
-  return <NewMeetupForm onAddMeetup={submitHandler} />;
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeetup;
