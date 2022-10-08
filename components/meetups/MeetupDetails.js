254ebf98021f35124b8d46b05cff1a3213b0a2f4
vishnu vp <vishnuprakaash257@gmail.com>
2022-10-08 21:35:18 +0530

    fix bugs

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -5,8 +5,8 @@ import classes from "./MeetupDetails.module.css";
 function MeetupDetails(props) {
   return (
     <>
-      <img src={props.image} alt={props.title} class={classes.image} />
-      <h1 class={classes.center}>{props.title}</h1>
+      <img src={props.image} alt={props.title} className={classes.image} />
+      <h1 className={classes.center}>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
     </>
